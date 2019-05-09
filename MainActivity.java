35dee51d83084b06cc4cdfcc34a51c96fb772ad4
avelinoecija <43956938+avelinoecija@users.noreply.github.com>
2019-05-09 17:06:34 +0200

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -4,22 +4,21 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -27,6 +26,8 @@ import butterknife.InjectView;
 public class MainActivity extends Activity implements OnClickListener {
 
     private static final int REQUEST_SIGNUP = 0;
+    private String TAG = MainActivity.class.getSimpleName();
+
 
     @InjectView(R.id.input_email)
     EditText _emailText;
@@ -36,6 +37,136 @@ public class MainActivity extends Activity implements OnClickListener {
     Button _loginButton;
     @InjectView(R.id.link_signup)
     TextView _signupLink;
+    private ListView lv;
+
+    ArrayList<HashMap<String, String>> contactList;
+    @Override
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        lv = (ListView) findViewById(R.id.list);
+        contactList = new ArrayList<>();
+
+
+
+        new GetContacts().execute();
+
+        _loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        _signupLink.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Start the Signup activity
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivityForResult(intent, REQUEST_SIGNUP);
+            }
+        });
+
+
+    }
+
+    private class GetContacts extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            HttpHandler sh = new HttpHandler();
+            // Making a request to url and getting response
+            String url = "http://192.168.202.191:8000/api/usuarios";
+            String jsonStr = sh.makeServiceCall(url);
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+
+                    // Getting JSON Array node
+                    JSONArray contacts = jsonObj.getJSONArray("data");
+
+
+                    // looping through All Contacts
+                    for (int i = 0; i < contacts.length(); i++) {
+                        JSONObject c = contacts.getJSONObject(i);
+                        String email = c.getString("correo");
+                        String pass = c.getString("contrasena");
+                        String iduser = c.getString("idusuario");
+
+
+                        // tmp hash map for single contact
+                        HashMap<String, String> contact = new HashMap<>();
+
+
+                        // adding each child node to HashMap key => value
+
+                        contact.put("correo", email);
+                        contact.put("contrasena", pass);
+                        contact.put("idusuario", iduser);
+
+
+                        // adding contact to contact list
+                        contactList.add(contact);
+
+
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                }
+
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            Toast.makeText(MainActivity.this, "cargando...", Toast.LENGTH_LONG).show();
+
+
+
+        }
+       @Override public void onPostExecute(Void results) {
+           super.onPostExecute(results);
+
+            Button b = (Button) findViewById(R.id.my_button);
+            b.setClickable(true);
+
+
+
+
+       }
+
+    }
 
     @Override
     public void onClick(View arg0) {
@@ -44,7 +175,8 @@ public class MainActivity extends Activity implements OnClickListener {
 
         b.setClickable(false);
 
-        new LongRunningGetIO().execute();
+
+
     }
 
     public void onLoginFailed() {
@@ -58,6 +190,7 @@ public class MainActivity extends Activity implements OnClickListener {
             boolean valid = true;
             String email = _emailText.getText().toString();
             String pass = _passwordText.getText().toString();
+
             if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
                 _emailText.setError("Introduzca una dirección de correo válida");
                 valid = false;
@@ -71,37 +204,15 @@ public class MainActivity extends Activity implements OnClickListener {
                 _passwordText.setError(null);
             }
 
-
             return valid;
+
         }
 
 
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            ButterKnife.inject(this);
-
-            _loginButton.setOnClickListener(new View.OnClickListener() {
-
-                @Override
-                public void onClick(View v) {
-                    login();
-                }
-            });
-
-            _signupLink.setOnClickListener(new View.OnClickListener() {
-
-                @Override
-                public void onClick(View v) {
-                    // Start the Signup activity
-                    Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
-                    startActivityForResult(intent, REQUEST_SIGNUP);
-                }
-            });
-        }
 
         public void login() {
+
+
 
             if (!validate()) {
                 onLoginFailed();
@@ -112,78 +223,10 @@ public class MainActivity extends Activity implements OnClickListener {
             }
         }
 
-        protected void onPostExecute(String results) {
-            if (results != null) {
-
-                EditText et = (EditText) findViewById(R.id.my_button);
-
-                et.setText(results);
-
-            }
-
-            Button b = (Button) findViewById(R.id.my_button);
-
-            b.setClickable(true);
-
-        }
-
-        public class LongRunningGetIO extends AsyncTask<Void, Void, String> {
-            public String getASCIIContentFromEntity(InputStream entity) throws IllegalStateException, IOException {
-
-                InputStream in = entity;
-
-                StringBuffer out = new StringBuffer();
-                int n = 1;
-                while (n > 0) {
-                    byte[] b = new byte[4096];
-
-                    n = in.read(b);
-
-                    if (n > 0) out.append(new String(b, 0, n));
-
-                }
-
-                return out.toString();
-
-            }
-
-            public String doInBackground(Void... params) {
-                BufferedReader reader = null;
-                String correo = null;
-                String clave = null;
-                JSONObject c = null;
-                try {
-                    URL githubEndpoint = new URL("http://192.168.202.191:8000/api/usuarios");
-                    HttpURLConnection myConnection = (HttpURLConnection) githubEndpoint.openConnection();
-                    InputStream responseBody = myConnection.getInputStream();
-                    reader = new BufferedReader(new InputStreamReader(responseBody));
-                    StringBuffer bufer = new StringBuffer();
-                    String line = "";
-                    while ((line = reader.readLine()) != null) {
-                        bufer.append(line);
-                    }
-                    String finalJson = bufer.toString();
-                    JSONObject json = new JSONObject(finalJson);
-                    JSONArray lista = json.getJSONArray("data");
-                    //correo = lista.getJSONArray(0);
-                    for (int i = 0; i < lista.length(); i++) {
-                        c = lista.getJSONObject(i);
-                        correo = c.getString("correo");
-                        clave = c.getString("contrasena");
-                    }
 
 
-                } catch (Exception e) {
-                    return e.getLocalizedMessage();
-
-                }
-
-                return correo;
-
-            }
-
-        }
     }
+
 
 
 
